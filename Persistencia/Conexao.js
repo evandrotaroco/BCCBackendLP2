6515c0a0442a44e626ff3dd20710d5389a335ed5
Evandro Taroco <evandrotaroco@hotmail.com>
2024-11-07 18:51:15 -0300

    Update Conexao.js

--- Persistencia/Conexao.js
+++ Persistencia/Conexao.js
@@ -11,11 +11,11 @@ export default async function conectar(){
     }
     else{
         global.poolConexoes = await mysql.createPool({
-            "host":process.env.HOST_TESTE,
-            "port":process.env.PORTA_TESTE,
-            "database":process.env.DATABASE_TESTE,
-            "user":process.env.USER_TESTE,
-            "password":process.env.PASSWORD_TESTE,
+            "host":process.env.IP_BANCO_DE_DADOS,
+            "port":process.env.PORTA_BANCO_DE_DADOS,
+            "database":process.env.BASE_DE_DADOS,
+            "user":process.env.BD_USUARIO,
+            "password":process.env.BD_SENHA,
             "connectTimeout":60000,
             "waitForConnections":true,
             "queueLimit":20
